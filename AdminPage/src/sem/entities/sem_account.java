c6765dc976a7d409d0ed5cbbe7d539ac84537e4d
Ngô Cao Sơn Hải <81684281+NgoCaoSonHai@users.noreply.github.com>
2021-07-31 21:57:17 +0700

    entities

---
+++ AdminPage/src/sem/entities/sem_account.java
@@ -0,0 +1,91 @@
+package sem.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sem_account")
+public class sem_account {
+	@Id
+	@Column(name =  "id")
+	private int id;
+	
+	@Column(name =  "name")
+	private String name;
+	
+	@Column(name = "username")
+	private String username;
+	
+	@Column(name = "password")
+	private String password;
+	
+	public sem_account() {
+		super();
+	}
+	
+	public sem_account(int id, String name, String username, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.username = username;
+		this.password = password;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ AdminPage/src/sem/entities/sem_cart.java
@@ -0,0 +1,95 @@
+package sem.entities;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sem_cart")
+public class sem_cart {
+	@Id
+	@Column(name =  "id")
+	private int id;
+	
+	@Column(name = "customer")
+	private int customer;
+	
+	@Column(name = "datecreate")
+	private Date datecreate;
+	
+	@OneToMany(mappedBy = "cart")
+	private List<sem_cart_book> sem_cart_books;
+
+	public sem_cart() {
+		super();
+	}
+
+	public sem_cart(int id, int customer, Date datecreate, List<sem_cart_book> sem_cart_books) {
+		super();
+		this.id = id;
+		this.customer = customer;
+		this.datecreate = datecreate;
+		this.sem_cart_books = sem_cart_books;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the customer
+	 */
+	public int getCustomer() {
+		return customer;
+	}
+
+	/**
+	 * @param customer the customer to set
+	 */
+	public void setCustomer(int customer) {
+		this.customer = customer;
+	}
+
+	/**
+	 * @return the datecreate
+	 */
+	public Date getDatecreate() {
+		return datecreate;
+	}
+
+	/**
+	 * @param datecreate the datecreate to set
+	 */
+	public void setDatecreate(Date datecreate) {
+		this.datecreate = datecreate;
+	}
+
+	/**
+	 * @return the sem_cart_books
+	 */
+	public List<sem_cart_book> getSem_cart_books() {
+		return sem_cart_books;
+	}
+
+	/**
+	 * @param sem_cart_books the sem_cart_books to set
+	 */
+	public void setSem_cart_books(List<sem_cart_book> sem_cart_books) {
+		this.sem_cart_books = sem_cart_books;
+	}
+}
