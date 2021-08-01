4a3122bf3f3d21835f9deed0899405aeaeebf594
Tan098 <tanvcm1998@gmail.com>
2021-08-01 22:08:11 +0700

    book entities

---
+++ AdminPage/src/sem/dao/impl/BookDAOimpl.java
@@ -0,0 +1,114 @@
+package sem.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import sem.dao.BookDAO;
+import sem.entities.sem_book;
+@Repository
+public class BookDAOimpl implements BookDAO{
+	@Autowired
+	private SessionFactory sessionFactory;
+	@Override
+	public List<sem_book> getBooks(Integer offset, Integer maxResult) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.openSession();
+		try {
+			session.beginTransaction();
+			List list = session.createQuery("from sem_book")
+					.setFirstResult(offset)
+					.setMaxResults(maxResult)
+					.list();
+			session.getTransaction().commit();
+			session.close();
+			return list;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			session.close();
+		}
+		return null;
+	}
+	@Override
+	public boolean insertBook(sem_book b) {
+		Session session = sessionFactory.openSession();
+		try {
+			session.beginTransaction();
+			session.save(b);
+			session.getTransaction().commit();
+			session.close();
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			session.close();
+		}
+		return false;
+	}
+	@Override
+	public sem_book getBookById(Integer id) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.openSession();
+		try {
+			session.beginTransaction();
+			sem_book b = (sem_book) session.createQuery("from sem_book where id = :id")
+			.setParameter("id", id)
+			.uniqueResult();
+			session.getTransaction().commit();
+			session.close();
+			return b;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			session.close();
+		}
+		return null;
+	}
+	@Override
+	public boolean updateBook(sem_book b) {
+		Session session = sessionFactory.openSession();
+		try {
+			session.beginTransaction();
+			session.update(b);
+			session.getTransaction().commit();
+			session.close();
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			session.close();
+		}
+		return false;
+	}
+	@Override
+	public boolean deleteBook(Integer id) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.openSession();
+		try {
+			session.beginTransaction();
+			int i = session.createQuery("delete from sem_book where id = :id")
+			.setParameter("id",id)
+			.executeUpdate();
+			session.getTransaction().commit();
+			session.close();
+			if (i>0) 
+				return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			session.close();
+		}
+		return false;
+	}
+
+
+}
