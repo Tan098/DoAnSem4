e917cc8659623ff8c185ce68fd60e47be8bae28e
Tan098 <tanvcm1998@gmail.com>
2021-08-02 22:45:13 +0700

    Accoung,author

---
+++ AdminPage/src/sem/dao/AccountDAO.java
@@ -0,0 +1,13 @@
+package sem.dao;
+
+import java.util.List;
+
+import sem.entities.sem_account;
+
+public interface AccountDAO {
+	public List<sem_account> getAccounts();
+	public boolean insertAccount(sem_account a);
+	public boolean updateAccount(sem_account a);
+	public boolean deleteAccount(Integer id);
+	public sem_account getAccountById(Integer id);
+}
